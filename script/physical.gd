b8944c9c2d6b882164202c3db47f0beb50d6ddbe
Laora <lola.mitou@gmail.com>
2025-01-15 13:44:48 +0100

    Fixed bugs and polish the wip

--- script/physical.gd
+++ script/physical.gd
@@ -3,8 +3,9 @@ extends VBoxContainer
 # Variables pour les LineEdit nodes
 @export var line_edit_strength: LineEdit
 @export var line_edit_speed: LineEdit
-@export var line_edit_defense: LineEdit
-@export var line_edit_healthmax: LineEdit
+
+@export var label_defense: Label
+@export var label_healthmax: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void: 
@@ -20,8 +21,8 @@ func _on_line_edit_text_changed(new_text):
 	var defense = 10 + num_strength + num_speed
 	var healthmax = 10 + num_strength
 
-	line_edit_defense.text = str(defense)
-	line_edit_healthmax.text = str(healthmax)
+	label_defense.text = str(defense)
+	label_healthmax.text = "/" + str(healthmax)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
